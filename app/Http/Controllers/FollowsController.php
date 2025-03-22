44dfbc94ca7b603f73ed613e93c45bb68cb34a51
kida0214 <kidawenzi@gmail.com>
2025-03-22 17:47:17 +0900

    コミットテスト

--- app/Http/Controllers/FollowsController.php
+++ app/Http/Controllers/FollowsController.php
@@ -3,14 +3,68 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Models\Post;
+use App\Models\Follow;
 
 class FollowsController extends Controller
 {
-    //
-    public function followList(){
-        return view('follows.followList');
+    // フォロー中のユーザー一覧を表示
+    public function followList()
+    {
+        $user = Auth::user();
+
+        // フォローしているユーザーの取得
+        $followingUsers = $user->following;
+
+        // フォローしているユーザーの投稿を取得
+        $followedPosts = Post::whereIn('user_id', $followingUsers->pluck('id'))->latest()->get();
+
+        // ビューにデータを渡す
+        return view('follows.followList', compact('followingUsers', 'followedPosts'));
     }
-    public function followerList(){
-        return view('follows.followerList');
+
+    // フォロワー一覧を表示
+    public function followerList()
+    {
+        $user = Auth::user();
+
+        // フォロワーの取得
+        $followers = $user->followers;
+
+        // フォロワーの投稿を取得
+        $followerPosts = Post::whereIn('user_id', $followers->pluck('id'))->latest()->get();
+
+        return view('follows.followerList', compact('followers', 'followerPosts'));
+    }
+
+    // ユーザーをフォローする処理
+    public function follow($id)
+    {
+        $user = Auth::user();
+
+        // すでにフォローしているかチェック
+        if (!$user->isFollowing($id)) {
+            Follow::create([
+                'following_id' => $user->id,
+                'followed_id' => $id,
+            ]);
+        }
+
+        return back();
+    }
+
+    // フォローを解除する処理
+    public function unfollow($id)
+    {
+        $user = Auth::user();
+
+        // フォロー解除処理
+        Follow::where('following_id', $user->id)
+            ->where('followed_id', $id)
+            ->delete();
+
+        return back();
     }
 }
